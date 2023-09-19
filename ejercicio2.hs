ccf0c9f9617a740c895034999bb4a7f69d75a231
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-19 02:51:47 -0300

    HICE EL 2.2

--- ejercicio2.hs
+++ ejercicio2.hs
@@ -9,15 +9,111 @@ pertenece n (x:xs) | n == x   = True
 
 --o 
 pertenece2 :: (Eq t) => t ->[t] -> Bool
-pertenece2 n (x:[])| n/=x  = False  
+pertenece2 n (x:[]) | n/=x  = False  
 pertenece2 n (x:xs) | n == x   = True 
-                   | otherwise = pertenece2 n (xs) 
+                    | otherwise = pertenece2 n (xs) 
 
 --o 
 pertenece3 :: (Eq t) => t -> [t] -> Bool
 pertenece3 _ [] = False
-pertenece3 n (x:xs)
-    | x == n = True
-    | otherwise = n `pertenece3` xs
+pertenece3 n (x:xs) | x == n = True
+                    | otherwise = n `pertenece3` xs
+
+----------------------------------------------------------
+
+--2.2)todosIguales :: (Eq t) => [t] -> Bool,
+--todosIguales :: (Eq t) => [t] -> Bool, que dada una lista devuelve verdadero 
+--si y solamente si todos sus elementos son iguales
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = True
+todosIguales [x] = True
+todosIguales (x:xs) = (x == head xs)  && todosIguales (xs)  
 
 
+-------------------------------------------------------------------------
+--2.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------
+--compararUnoConLaLista :: (Eq t) => t -> [t] -> Bool
+--compararUnoConLaLista x [] = False
+--compararUnoConLaLista x (y:ys)  | x == y = True
+--                                |otherwise = compararUnoConLaLista x (ys)
+
+
+--todosIguales :: (Eq t) => [t] -> Bool 
+--todosIguales (x:[]) = False  
+--todosIguales (x:xs) | compararUnoConLaLista x (xs) == True = True 
+    --                |otherwise = todosIguales(xs)
+
+-----------------------------------------------------------------
