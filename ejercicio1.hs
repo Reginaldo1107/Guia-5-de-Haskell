2af2a5882ca6ad98e298a5ed85689f7095f2d32b
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-24 15:06:38 -0300

    Actualice y mejore los ejercicios 1 y 2 , arrancamos con el 3

--- ejercicio1.hs
+++ ejercicio1.hs
@@ -16,6 +16,13 @@ ultimo :: [t] -> t
 ultimo (x:[]) = x 
 ultimo (x:xs) = ultimo (xs) 
 
+
+ultimo2 :: [t] -> t 
+
+ultimo2 [x] = x 
+ultimo2 (x:xs) = ultimo2(xs)
+          
+
 --3. principio :: [t] -> [t]
 -- segun la siguiente especificacion:
 -- resultado = subseq(s, 0, |s| - 1) 
@@ -39,6 +46,42 @@ principio2 (x:xs) = x : principio2 xs
 reverso :: [t] -> [t]
 reverso (x:[]) =[x]
 reverso  (x:xs) = reverso(xs) ++ [x]
+---------------------------------------
+
+--2. ultimo :: [t] -> t segun la siguiente especificaci´on:
+--problema ultimo (s: seq⟨T⟩) : T {
+--requiere: { |s| > 0 }
+--asegura: { resultado = s[|s| − 1] }
+--}
+
+
+
+
+
+--3. principio :: [t] -> [t]
+-- segun la siguiente especificacion:
+-- resultado = subseq(s, 0, |s| - 1) 
+--subseq(s, 0, |s| - 1) = ME DEVUELVE UNA SECUNCIA SACANDOME UN ELEMNTO DE INDICE |S|-1 
+--EMPEZANDO POR 0 , RECORDAR QUE LA SECUENCIA EMPIEZA POR 0 ,1,2,3,4
+--ejemplo "HOLA" -> "HOL"
+principio3 :: (Eq t ) =>[t] -> [t]
+principio3 [] = []
+principio3 [x] = []
+principio3 (x:xs) = x : principio3 (xs)
+
+
+
+--reverso :: [t] -> [t] segun la siguiente especificacion:
+--problema reverso (s: seq⟨T⟩) : seq⟨T⟩
+-- resultado tiene los mismos elementos que s 
+--pero en orden inverso.
+--ejemplo : "HOLA" --> "ALOH"
+reverso2 :: [t] -> [t]
+reverso2 [] = []
+reverso2 (x:xs) =  reverso2(xs) ++ [x]
+
+
+
 
 
 
--- ejercicio2.hs
+++ ejercicio2.hs
@@ -4,32 +4,25 @@
 --la siguiente especificacion:
 --Eq es el conjunto de tipos de datps a los que se le puede aplicar {Bool Int Integer Float Double ...}
 --Devuelve True si el elemento esta en la lista
-pertenece :: (Eq t) => t ->[t] -> Bool
-pertenece n []  = False  
-pertenece n (x:xs) | n == x   = True 
-                   | otherwise = pertenece n (xs) 
-
---o 
-pertenece2 :: (Eq t) => t ->[t] -> Bool
-pertenece2 n (x:[]) | n/=x  = False  
-pertenece2 n (x:xs) | n == x   = True 
-                    | otherwise = pertenece2 n (xs) 
-
---o 
-pertenece3 :: (Eq t) => t -> [t] -> Bool
-pertenece3 _ [] = False
-pertenece3 n (x:xs) | x == n = True
-                    | otherwise = n `pertenece3` xs
+pertenece :: (Eq t ) => t -> [t] -> Bool
+pertenece _ [] = False --Hay 0 elementos
+pertenece i [x] = (i == x )   -- Hay 1 elemento , este puede ir como no tambien porque esta el caso base del vacio  
+pertenece i (x:xs)  | i == x = True
+                    |otherwise = pertenece i (xs)
 
 ----------------------------------------------------------
 
 --2.2)todosIguales :: (Eq t) => [t] -> Bool,
 --todosIguales :: (Eq t) => [t] -> Bool, que dada una lista devuelve verdadero 
 --si y solamente si todos sus elementos son iguales
+
 todosIguales :: (Eq t) => [t] -> Bool
-todosIguales [] = True
+todosIguales [] = True -- Suponiendo que si no hay nada tambien son iguales por en una lista vacia no hay elementos entonces son iguales sus elementos 
 todosIguales [x] = True
-todosIguales (x:xs) = (x == head xs)  && todosIguales (xs)  
+todosIguales (x:[y])   |x == y = True 
+                        |otherwise = False
+todosIguales (x:xs) | pertenece x (xs) == True = todosIguales (xs)
+                    |otherwise = False
 
 
 -------------------------------------------------------------------------
@@ -38,19 +31,18 @@ todosIguales (x:xs) = (x == head xs)  && todosIguales (xs)
 --segun la siguiente especicacion:
 --problema todosDistintos (s: seq⟨T⟩) : B 
 --requiere:{True}
---asegura:{resultado = false ↔ existen dos posiciones distintas de s con igual valor }0
---Si devuelve true es porque son ditintos 
---si devuelve false es porque el elemento es igual al de la lista
-verificarQueSeanDistintos :: (Eq t) => t -> [t] -> Bool
-verificarQueSeanDistintos x [] = True
-verificarQueSeanDistintos x  (y:ys) |x == y  = False
-                                    | otherwise = verificarQueSeanDistintos x (ys)
+--asegura:{resultado = false ↔ existen dos posiciones distintas de s con igual valor }
+--Si devuelve true es porque son ditintos todos los elementos 
+--si devuelve false es porque un par de elementos son iguales 
+todosDistintos2 :: (Eq t) => [t] -> Bool
+todosDistintos2 [] = True -- Suponiendo que la lista vacia tiene elementos distintos por tener 0 elementos
+todosDistintos2 [x] =True -- Como es 1 elemento tiene sus elementos distintos 
+todosDistintos2 (x:[y])  | x /= y  = True
+                        |otherwise = False --Con 2 elementos
+todosDistintos2 (x:xs)  | pertenece x xs == True = False
+                        |otherwise = todosDistintos2 (xs)
 
-todosDistintos :: (Eq t) => [t] -> Bool
-todosDistintos [] = True
-todosDistintos [x] = True
-todosDistintos (x:xs) | verificarQueSeanDistintos x xs == False  = False
-                      |otherwise = todosDistintos (xs)
+
 
 ---------------------------------------------------------------------
 --2.4 ) 4. hayRepetidos :: (Eq t) => [t] -> Bool seg´un la siguiente especicaci´on:
@@ -58,45 +50,27 @@ todosDistintos (x:xs) | verificarQueSeanDistintos x xs == False  = False
 --requiere: { True }
 --asegura: { resultado = true ↔ existen dos posiciones distintas de s con igual valor }
 --}
-verificarQueSeanIguales :: (Eq t) => t -> [t] -> Bool
-verificarQueSeanIguales x [] = False
-verificarQueSeanIguales x  (y:ys) |x == y  = True
-                                    | otherwise = verificarQueSeanIguales x (ys)
 
-hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos [] = False
-hayRepetidos [x] = False
-hayRepetidos (x:xs) | verificarQueSeanIguales x xs == True  = True
-                      |otherwise = hayRepetidos (xs)
+hayRepetidos2 :: (Eq t) => [t] ->Bool
+hayRepetidos2 [] = False --tomemos que si esta vacio no es repetido 
+hayRepetidos2 [x] = False  
+hayRepetidos2 (x:xs) | pertenece x (xs) == True = True
+                    |otherwise = hayRepetidos2 (xs)
 
-----------------------------------------------------------------------------------
+
 --5. quitar :: (Eq t) => t -> [t] -> [t], 
 --que dados un entero x y una lista xs, 
 --elimina la primera aparicion de x en
 --la lista xs (de haberla).
-recorrerLaLista :: (Eq t) => [t]->[t]
-recorrerLaLista[] = []
-recorrerLaLista (x:[]) = [x]
-recorrerLaLista (x:xs) = x:recorrerLaLista(xs)
-
 quitar :: (Eq t) => t -> [t] -> [t]
-quitar x [] = []
-quitar x [y] |x==y = []
-             |otherwise = [y]
-quitar x (y:ys) | x == y = recorrerLaLista(ys)
-                |otherwise = y:(quitar x (ys))             
+quitar _ [] = []
+quitar x [y]    | x == y = []
+                |otherwise = [y]
 
+quitar i (x:xs)     | i == x = (xs) 
+                    |otherwise =  x:(quitar i (xs)) 
+----------------------------------------------------------------------------------
 
---o
-
-quitar2 :: (Eq t) => t -> [t] -> [t]
-quitar2 _ [] = []
-quitar2 x (y:xs) | x == y = xs
-                | otherwise = y : quitar2 x xs
-
---Quiero entrar buscando de a uno x:(xs)
-
---------------------------------------
 
 --6. quitarTodos :: (Eq t) => t -> [t] -> [t],
 --que dados un entero x y una lista xs,
@@ -107,19 +81,82 @@ quitar2 x (y:xs) | x == y = xs
 --asegura: { resultado es igual a s pero sin el elemento e. }
 quitarTodos :: (Eq t) => t -> [t] -> [t]
 quitarTodos _ [] = []
-quitarTodos x (y:ys)    |x == y  = quitarTodos x ys
-                        |otherwise = y:(quitarTodos  x ys)
---o
+quitarTodos x [y]   | x==y = []
+                    |otherwise = [y]
+quitarTodos i (x:xs)    | i == x  = quitarTodos i (xs)
+                        | otherwise = x :(quitarTodos i (xs))    
 
-quitarTodos2 :: (Eq t ) => t -> [t] -> [t]
-quitarTodos2 _ [] = []
-quitarTodos2 x (y:xs)
-    | x == y && noPertenece = xs
-    | x == y = quitarTodos2 x xs
-    | otherwise = y : quitarTodos2 x xs
-    where noPertenece = not (x `pertenece` (y:xs))
 
+----------------------------------------------------------------------------------
+--7. eliminarRepetidos :: (Eq t) => [t] -> [t] 
+--que deja en la lista una unica aparicion de cada elemento, eliminando
+--las repeticiones adicionales.
+
+eliminarRepetidos2 :: (Eq t) => [t] -> [t]
+eliminarRepetidos2 [] = []
+eliminarRepetidos2 [x] = [x]
+eliminarRepetidos2 (x:[y])  | x == y = [x]
+                            |otherwise = (x:[y])
+eliminarRepetidos2 (x:xs)   |pertenece x (xs) == True  = x:eliminarRepetidos2 (quitarTodos x (xs))  
+                            |otherwise = x: eliminarRepetidos2 (xs)  
+
+
+--7. eliminarRepetidos :: (Eq t) => [t] -> [t] 
+--que deja en la lista una unica aparicion de cada elemento, eliminando
+--las repeticiones adicionales.
+sonIguales :: (Eq t) => t -> [t] -> Bool
+sonIguales x [] = False
+sonIguales x  (y:ys)|x == y  = True
+                    | otherwise = sonIguales x (ys)
+
+eliminarRepetidos1 :: (Eq t) => [t] -> [t]
+eliminarRepetidos1 [] = []
+eliminarRepetidos1 (x:xs)    | sonIguales x xs  = eliminarRepetidos1(xs) 
+                            |otherwise = x:eliminarRepetidos1(xs)
 --------------------------------------------------------------------------------
+--8. mismosElementos :: (Eq t) => [t] -> [t] -> Bool,
+-- que dadas dos listas devuelve verdadero si y solamente si
+--ambas listas contienen los mismos elementos, 
+--sin tener en cuenta repeticiones,
+--es decir:
+--problema mismosElementos (s: seq⟨T⟩, r: seq⟨T⟩) : B {
+--requiere: {True}
+--asegura: {resultado = true ↔ todo elemento de s pertenece r y viceversa}
+--}
+
+mismosElementos :: (Eq t) =>[t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos _  [] = False
+mismosElementos [] _ = False
+
+mismosElementos [x] [y] | x == y = True
+                        |otherwise = False
+mismosElementos (x:xs) (y:ys) | pertenece x (y:ys) = mismosElementos (quitarTodos x xs)  (quitarTodos x (y:ys))
+                        |otherwise = False
+
+---------------------------------------------------------------------------------------
+
+--9. capicua :: (Eq t) => [t] -> Bool segun la siguiente especificacion:
+--problema capicua (s: seq⟨T⟩) : Bool {
+--requiere: { True }
+--asegura: { (resultado = true) ↔ (s = reverso(s)) }
+--}
+--Por ejemplo capicua ['a','c', 'b', 'b', 'c', 'a'] es true
+--            capicua ['a', 'c', 'b', 'd', 'a']     es false
+--Da true si son iguales y false si no lo son 
+
+reverso :: [t] -> [t]
+reverso (x:[]) =[x]
+reverso  (x:xs) = reverso(xs) ++ [x]
+---------------------------------------
+
+capicua2 :: (Eq t ) => [t] ->Bool
+capicua2 [] = True
+capicua2 [x] = True
+capicua2 (x :[y])| x == y  = True
+                | otherwise = False
+capicua2 (x:xs)  | (x:xs) == reverso (x:xs) = True
+                |otherwise = False
 
 
 
@@ -137,40 +174,3 @@ quitarTodos2 x (y:xs)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---------------------------------------------------------
---compararUnoConLaLista :: (Eq t) => t -> [t] -> Bool
---compararUnoConLaLista x [] = False
---compararUnoConLaLista x (y:ys)  | x == y = True
---                                |otherwise = compararUnoConLaLista x (ys)
-
-
---todosIguales :: (Eq t) => [t] -> Bool 
---todosIguales (x:[]) = False  
---todosIguales (x:xs) | compararUnoConLaLista x (xs) == True = True 
-    --                |otherwise = todosIguales(xs)
-
------------------------------------------------------------------
